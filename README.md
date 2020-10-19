90793ae3541db82c82b6fc94f19f11d94d2ec7bc
adr810 <adr.flp810@gmail.com>
2020-10-18 22:42:23 -0300

    portfolio

---
+++ README.md
@@ -0,0 +1,16 @@
+A fork of  [rbhatia46 react portfolio](https://github.com/rbhatia46/React-Portfolio) with certifications sections and improved contact section, emphasis on mobile view, and flowing particles as background
+
+A simple SPA Portfolio template for developer/designers built with React. Use it to showcase your work, certfications and other information to clients.
+
+## [DEMO](https://its-mxc.github.io)
+
+## Preview
+![Preview](https://i.ibb.co/92Vrg2Q/Screenshot-2020-06-16-Uday-Nayak.png)
+
+## How to use
+1. Clone/Download the repo.
+2. Run  ``` npm install ```.
+3. Change the values in ```src/resumeData.js``` to suit your use-case.
+4. Run ```npm start``` to spin the up the local dev server port 3000.(http://localhost:3000).
+5. Make required changes in ```src/resumeData.js``` to suit your needs.
+
---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "André",
+  role: "Front end web developer",
+  roleDescription:
+    "",
+  socialLinks: [
+    {
+      name: "André Felipe",
+      url: "https://www.linkedin.com/in/andr%C3%A9-felipe-95468b191/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "André Felipe",
+      url: "https://github.com/adr810",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "Sou recém formado, em Análise e desenvolvimento de sistemas pela FMU. sou um desenvolvedor front end autodidata, fiz um estágio de 2 anos no INSS, onde eu desenvolvia landing pages usando HTML5, CSS e JavaScript.",
+  website: "its-mXc.github.io",
+  email: "adr.flp810@gmail.com",
+  address: "Bom Retiro, São Paulo",
+  resumelink:
+    "https://drive.google.com/uc?export=download&id=1DQYlMzyDo0p-eqfTyZGZWXzFmVXaVF8M",
+  education: [
+    {
+      UniversityName: "Faculdades Metropolitanas Unidas",
+      specialization: "tecnólogo",
+      MonthOfPassing: "Out",
+      YearOfPassing: "2019",
+      Achievements:
+        "Ajudei a realizar minha paixão em programação quando comecei o curso de Análise e desenvolvimento de sistemas, no 1º semestre com linguagem Java, algoritimos e logica de programação e nos semetres subsequentes foi apresentada a Estruturas de Dados, SGBD, Sistemas Operacionais, engenharia de software, gestão de projetos e computação para dispositivos móveis",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Instituto Nacional do Seguro Social",
+      specialization: "Estágio",
+      MonthOfJoining: "novembro",
+      YearOfJoining: "2017",
+      MonthOfLeaving: "setembro",
+      YearOfLeaving: "2019",
+      Achievements:
+        "Estagiário, desenvolvendo atividades de manutenção de computadores, instalaçãode softwares, suporte remoto a usuários e desenvolvimento de landing pages simples",
+    },
+    {
+      CompanyName: "Fundação CESP",
+      specialization: "Jovem Aprendiz",
+      MonthOfJoining: "Jan",
+      YearOfJoining: "2015",
+      MonthOfLeaving: "Feb",
+      YearOfLeaving: "2016",
+      Achievements:
+        "Jovem aprendiz, realizando atividades administrativas e operacionais",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "ReactJS",
+    },
+    {
+      skillname: "Html5",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "Css",
+    },
+    {
+      skillname: "Node",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Prashna",
+      description:
+        "Question asking platform inspired from quora built using Ruby on Rails",
+      imgurl: "images/portfolio/Prashna.png",
+      link: "https://prashna-uday.herokuapp.com/",
+      sourcecode: "https://github.com/its-mXc/prashna",
+    },
+    {
+      name: "ToDoList",
+      description:
+        "React SPA using firbase,context, and integration testing and node-sass",
+      imgurl: "images/portfolio/todolist.png",
+      link: "https://github.com/its-mXc/prashna",
+      sourcecode: "https://github.com/its-mXc/todolist",
+    },
+    {
+      name: "Quick Components",
+      description: "Components for Quick Protoyping in ReactJS",
+      imgurl: "images/portfolio/quick-components.png",
+      link: "https://github.com/its-mXc/quick-components",
+      sourcecode: "https://github.com/its-mXc/quick-components",
+    },
+    {
+      name: "React Portfolio Template",
+      description:
+        "Portfolio template to present your talents and projects, built using ReactJS",
+      imgurl: "images/portfolio/portfolio.png",
+      link: "https://its-mxc.github.io/",
+      link: "https://github.com/its-mXc/react-portfolio",
+      sourcecode: "https://github.com/its-mXc/react-portfolio",
+    },
+  ],
+};
+
+export default resumeData;
