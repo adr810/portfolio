e41044c570cd714645b630f1503ac43ae55d4588
adr810 <adr.flp810@gmail.com>
2021-01-30 05:36:00 -0300

    Updates

---
+++ precache-manifest.4ba73a244b944c53f126e2555d1c11c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "70cc0449f3182a4bc64e",
+    "url": "/react-portfolio/static/css/main.1f780065.chunk.css"
+  },
+  {
+    "revision": "70cc0449f3182a4bc64e",
+    "url": "/react-portfolio/static/js/main.70cc0449.chunk.js"
+  },
+  {
+    "revision": "d22b7ca17aec40ad6e68",
+    "url": "/react-portfolio/static/js/1.d22b7ca1.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "87fb98a045b1ff52cee7939326c9c308",
+    "url": "/react-portfolio/index.html"
+  }
+];
